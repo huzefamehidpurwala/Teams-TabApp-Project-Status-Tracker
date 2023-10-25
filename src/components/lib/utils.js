bb5b80292ae37d088432d85e6f5c76c591f6a8fc
huzefamehidpurwala <jamaliongithub@gmail.com>
2023-10-25 11:10:03 +0530

    compareObjects() created

--- src/components/lib/utils.js
+++ src/components/lib/utils.js
@@ -243,3 +243,20 @@ export function toTitleCase(str) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
+
+export function compareObjects(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
